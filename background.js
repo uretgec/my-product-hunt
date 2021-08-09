3b8256d56efeb3ccffe7ed4704420efc9e68edf9
uretgec <iletisim@uretgec.com>
2021-08-09 20:40:10 +0300

    feat: remove all comments and console.logs producthunt class added dirty lines rewritten

--- background.js
+++ background.js
@@ -1,32 +1,18 @@
-/* 
-    DOCS
-    - https://developer.chrome.com/docs/extensions/mv3/messaging/
-    - https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Intercept_HTTP_requests
-*/
-
-// Collect sending IAM_READY action message tabs
-//let iamReadyTabs = [];
+"use strict";
 
 // Manifest json file to object data
 let manifestData = chrome.runtime.getManifest()
 
 // Fired when the extension is first installed, when the extension is updated to a new version, and when the browser is updated to a new version.
-/*chrome.runtime.onInstalled.addListener(
+chrome.runtime.onInstalled.addListener(
     function() {
-        console.log("runtime.onInstalled", chrome.runtime.getManifest());
+        console.info('%c' + manifestData.name + ' Extension: %cWelcome to my world!', 'color: orange;', 'color: default;');
     }
-);*/
+);
 
 // When the user navigates to a new URL in a tab, this will typically generate several  onUpdated events as various properties of the tabs.Tab object are updated. This includes the url, but also potentially the title and favIconUrl properties. The status property will cycle through "loading" and "complete".
 chrome.tabs.onUpdated.addListener(
-    function (tabId, changeInfo, tab) {
-        // console.log("tabs.onUpdated", tabId, changeInfo, tab);
-        /*if (iamReadyTabs.includes(tabId) && tab.active && changeInfo.status === "complete" && tab.hasOwnProperty("url")) {
-            chrome.tabs.sendMessage(tabId, {
-                action: 'REMOVE_BLURRY_BLOCK'
-            });
-        }*/
-        
+    function (tabId, changeInfo, tab) {        
         if ( tab.active && changeInfo.status === "complete" && tab.hasOwnProperty("url") ) {
             chrome.permissions.contains({
                 permissions: manifestData.permissions,
@@ -34,7 +20,7 @@ chrome.tabs.onUpdated.addListener(
             }, function(result) {
                 if (result) {
                     chrome.tabs.sendMessage(tabId, {
-                        action: 'REMOVE_BLURRY_BLOCK'
+                        action: 'IM_READY'
                     });
                 } else {
                     // console.log("get out");
@@ -43,47 +29,3 @@ chrome.tabs.onUpdated.addListener(
         }
     }
 );
-
-// listen runtime message from content script or extension
-/*chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        // console.log("content script or extension sendMessage", request, sender, sendResponse);
-        if (request.action === "IAM_READY") {
-            if (sender.hasOwnProperty("tab")) {
-                iamReadyTabs.push(sender.tab.id);
-            }
-            sendResponse({action: "ROGER_THAT"});
-        }
-    }
-);*/
-
-/*
-chrome.webRequest.onBeforeRequest.addListener(
-    function(requestDetail) {
-        if (requestDetail.method === "POST") {
-            console.log("chrome.webRequest.onBeforeRequest", requestDetail);
-        }
-    },
-    { urls: ["https://www.producthunt.com/frontend/graphql"], types: ["xmlhttprequest"] },
-    ["requestBody", "extraHeaders"]
-);
-
-chrome.webRequest.onBeforeSendHeaders.addListener(
-    function(requestDetail) {
-        if (requestDetail.method === "POST") {
-            console.log("chrome.webRequest.onBeforeSendHeaders", requestDetail);
-        }
-    },
-    { urls: ["https://www.producthunt.com/frontend/graphql"], types: ["xmlhttprequest"] },
-    ["requestHeaders", "extraHeaders"]
-);
-
-
-chrome.webRequest.onCompleted.addListener(
-    function(requestDetail) {
-        console.log("chrome.webRequest.onCompleted", requestDetail);
-    },
-    { urls: ["https://www.producthunt.com/frontend/graphql"], types: ["xmlhttprequest"] },
-    ["responseHeaders", "extraHeaders"]
-);
-*/
