ffd2d52c1f222e814b1b587a485223b374e6799c
uretgec <iletisim@uretgec.com>
2021-10-10 13:16:25 +0300

    feat: dark mode icon style updated.

--- extension/lib/darkmode.js
+++ extension/lib/darkmode.js
@@ -9,16 +9,10 @@ MyDarkMode.prototype.DarkModePassive = "2";
 
 MyDarkMode.prototype.setMode = function (mode) {
     localStorage.setItem("myDarkMode", mode);
-    //chrome.storage.local.set({myDarkMode: mode});
 }
 
 MyDarkMode.prototype.getMode = function () {
     return localStorage.getItem("myDarkMode");
-    /*return new Promise( function (resolve) {
-        chrome.storage.local.get('myDarkMode', function (result) {
-            resolve(result.myDarkMode || "0");
-        });
-    });*/
 }
 
 // Via: https://css-tricks.com/a-complete-guide-to-dark-mode-on-the-web/
