be11b024540f7776fd1417dcf4e0c8bb03dd7683
uretgec <iletisim@uretgec.com>
2021-10-10 14:35:37 +0300

    feat: timeout function removed mouseover listener

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -50,80 +50,77 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 let prevPostItemId = null;
 document.addEventListener("mouseover", function (event) {
 
-    setTimeout(function () {
-        let relTarg = event.relatedTarget || event.toElement;
-        let postItemObj = relTarg.closest(myProductHunt.ProductPostBlock);
-        if(postItemObj === null) return;
+    let relTarg = event.relatedTarget || event.toElement;
+    let postItemObj = relTarg.closest(myProductHunt.ProductPostBlock);
+    if(postItemObj === null) return;
 
-        // Checked data-marked attribute
-        if(postItemObj.hasAttribute("data-marked")) return;
+    // Checked data-marked attribute
+    if(postItemObj.hasAttribute("data-marked")) return;
 
-        // Find producthunt id
-        let currentPostItemId = postItemObj.getAttribute("data-test").replace("post-item-","");
-        if(prevPostItemId === currentPostItemId) return;
-        prevPostItemId = currentPostItemId;
-        
-        let postItemUri = postItemObj.querySelector('a[data-test="post-name-' + currentPostItemId + '"]');
-        if(postItemUri === null) return;
-
-        let postItemSlug = postItemUri.getAttribute("href").replace("/posts/","");
-
-        // Extra content div created
-        let extraContentContainer = document.createElement("div");
-        extraContentContainer.setAttribute("class",myProductHunt.ExtraContentClassName);
-        
-        // Inject post item div
-        extraContentContainer.insertAdjacentHTML("beforeend", myProductHunt.createLoadingBarBlock());
-        postItemObj.append(extraContentContainer);
-        
-        // Graphql Post Request
-        fetch(
-            myProductHunt.generateUri("frontend", "graphql"), 
-            myProductHunt.generatePostRequestOptions(postItemSlug)
-        )
-        .then(function(resp) {
-            return resp.json();
-        })
-        .then(function(item) {
-            return myProductHunt.generateGraphqlResponseData(item);
-        })
-        .then(function(product) {
-
-            // Product marked after success response
-            postItemObj.setAttribute("data-marked", currentPostItemId);
-
-            // First Row: Hunter and Makers - Reviews + Socials
-            let extraContentBlock = myProductHunt.createRowContainerBlock(
-                myProductHunt.createHunterAndMakersBlock(product),
-                myProductHunt.createReviewsBlock(product),
-                myProductHunt.createSocialsBlock(product)
-            );
-
-            // Second Row: Headline
-            extraContentBlock += myProductHunt.createRowContainerBlock(
-                myProductHunt.createHeadlineBlock(product)
-            );
-
-            // Third Row: Topics
-            extraContentBlock += myProductHunt.createRowContainerBlock(
-                myProductHunt.createTopicsBlock(product)
-            );
-
-            // Fourth Row: Bookmarks
-            extraContentBlock += myProductHunt.createRowContainerBlock(
-                myProductHunt.createBookmarksBlock(product)
-            );
-
-            extraContentContainer.innerHTML = ''; // Sorry about that
-            extraContentContainer.insertAdjacentHTML("beforeend", extraContentBlock);
-            postItemObj.append(extraContentContainer);
-
-        })
-        .catch(function(e) {
-            console.warn("fetch Error", e);
-        });
-    }, 500);
+    // Find producthunt id
+    let currentPostItemId = postItemObj.getAttribute("data-test").replace("post-item-","");
+    if(prevPostItemId === currentPostItemId) return;
+    prevPostItemId = currentPostItemId;
     
+    let postItemUri = postItemObj.querySelector('a[data-test="post-name-' + currentPostItemId + '"]');
+    if(postItemUri === null) return;
+
+    let postItemSlug = postItemUri.getAttribute("href").replace("/posts/","");
+
+    // Extra content div created
+    let extraContentContainer = document.createElement("div");
+    extraContentContainer.setAttribute("class",myProductHunt.ExtraContentClassName);
+    
+    // Inject post item div
+    extraContentContainer.insertAdjacentHTML("beforeend", myProductHunt.createLoadingBarBlock());
+    postItemObj.append(extraContentContainer);
+    
+    // Graphql Post Request
+    fetch(
+        myProductHunt.generateUri("frontend", "graphql"), 
+        myProductHunt.generatePostRequestOptions(postItemSlug)
+    )
+    .then(function(resp) {
+        return resp.json();
+    })
+    .then(function(item) {
+        return myProductHunt.generateGraphqlResponseData(item);
+    })
+    .then(function(product) {
+
+        // Product marked after success response
+        postItemObj.setAttribute("data-marked", currentPostItemId);
+
+        // First Row: Hunter and Makers - Reviews + Socials
+        let extraContentBlock = myProductHunt.createRowContainerBlock(
+            myProductHunt.createHunterAndMakersBlock(product),
+            myProductHunt.createReviewsBlock(product),
+            myProductHunt.createSocialsBlock(product)
+        );
+
+        // Second Row: Headline
+        extraContentBlock += myProductHunt.createRowContainerBlock(
+            myProductHunt.createHeadlineBlock(product)
+        );
+
+        // Third Row: Topics
+        extraContentBlock += myProductHunt.createRowContainerBlock(
+            myProductHunt.createTopicsBlock(product)
+        );
+
+        // Fourth Row: Bookmarks
+        extraContentBlock += myProductHunt.createRowContainerBlock(
+            myProductHunt.createBookmarksBlock(product)
+        );
+
+        extraContentContainer.innerHTML = ''; // Sorry about that
+        extraContentContainer.insertAdjacentHTML("beforeend", extraContentBlock);
+        postItemObj.append(extraContentContainer);
+
+    })
+    .catch(function(e) {
+        console.warn("fetch Error", e);
+    });    
 }, false);
 
 // Toggle DarkMode
