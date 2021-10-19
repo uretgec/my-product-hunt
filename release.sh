fae126a3bfb75a2aecf21d66243a4311bc4c65d9
uretgec <iletisim@uretgec.com>
2021-10-19 03:04:43 +0300

    feat: firefox v2 manifest file added. release script updated for two different manifest version

--- release.sh
+++ release.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# Usage: ./release new-build-version
-# Usage: ./release 1.0.6
-# Before run: chmod a+x runme.sh
+# Usage: ./release new-build-version manifest-version
+# Usage: ./release 1.0.6 3
+# Before run: chmod a+x release.sh
 # Local Variables
 ARGS=("$@")
 WHEREIAM=$(pwd)
 BUILD_NUMBER_OLD="$(cat $WHEREIAM/VERSION)"
 BUILD_NUMBER_NEW="${ARGS[0]}"
+MANIFEST_VERSION="${ARGS[1]}" # 3: Chrome, 2: Firefox
 
 # sed Checker
 if [ -x "$(command -v sed)" ]
 then
 
-    sed -e 's|BUILD_NUMBER|'$BUILD_NUMBER_NEW'|g' $WHEREIAM/manifest.template > $WHEREIAM/extension/manifest.json
+    sed -e 's|BUILD_NUMBER|'$BUILD_NUMBER_NEW'|g' $WHEREIAM/manifestv$MANIFEST_VERSION.template > $WHEREIAM/extension/manifest.json
     echo "Extension version updated from $BUILD_NUMBER_OLD to $BUILD_NUMBER_NEW"
     echo $BUILD_NUMBER_NEW > $WHEREIAM/VERSION
     echo "VERSION file updated from $BUILD_NUMBER_OLD to $BUILD_NUMBER_NEW"
@@ -28,8 +29,8 @@ fi
 if [ -x "$(command -v zip)" ]
 then
     cd $WHEREIAM/extension
-    zip -r $WHEREIAM/build/my-product-hunt-$BUILD_NUMBER_NEW.zip * -x "*.DS_Store" -x "__MACOSX" -x "*_original.css"
-    echo "Extension folder compressed: my-product-hunt-$BUILD_NUMBER_NEW.zip"
+    zip -r $WHEREIAM/build/my-product-hunt-$BUILD_NUMBER_NEW-v$MANIFEST_VERSION.zip * -x "*.DS_Store" -x "__MACOSX" -x "*_original.css"
+    echo "Extension folder compressed: my-product-hunt-$BUILD_NUMBER_NEW-v$MANIFEST_VERSION.zip"
 
 else
 
