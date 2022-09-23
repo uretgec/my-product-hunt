7932a8a89758576a9b0498a641a222a93e2e3699
uretgec <iletisim@uretgec.com>
2022-09-24 00:06:44 +0300

    feat: manifest files updated dev manifest file add license year update version upgrade dark mode style file old lines remove readme update

--- sh/build.sh
+++ sh/build.sh
@@ -7,14 +7,14 @@ BUILD_NUMBER_NEW="${ARGS[1]}"
 # Development Build
 # Via: https://developer.chrome.com/docs/webstore/cws-dashboard-distribution/#publishing-a-test-version
 echo "Generator Development: Firefox Manifest v2 zip file"
-./release.sh $ENV $BUILD_NUMBER_NEW 2
+./sh/release.sh $ENV $BUILD_NUMBER_NEW 2.dev
 
 echo "Generator Development: Chrome Manifest v3 zip file"
-./release.sh $ENV $BUILD_NUMBER_NEW 3
+./sh/release.sh $ENV $BUILD_NUMBER_NEW 3.dev
 
 # Production Build
 echo "Generator: Firefox Manifest v2 zip file"
-./release.sh $ENV $BUILD_NUMBER_NEW 2
+./sh/release.sh $ENV $BUILD_NUMBER_NEW 2
 
 echo "Generator: Chrome Manifest v3 zip file"
-./release.sh $ENV $BUILD_NUMBER_NEW 3
+./sh/release.sh $ENV $BUILD_NUMBER_NEW 3
